f58ed07d770be70e45c996b6521792dddf25ca82
Remuraro <79248198+Remuraro@users.noreply.github.com>
2021-04-14 21:25:31 +0100

    updated app.js

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ mongoose.connect(mongoURL, {useNewUrlParser: true}).then(()=>{  // updated the c
     app.listen(3000, ()=>{
         console.log("CA API started on port 3000, test using http://localhost:3000");
     });
-});
+})
+
+/*const express = require("express");
+const cors = require('cors');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+console.log(process.argv);
+
+mongoose.connect("mongodb://localhost:27017/ca-db", {useNewUrlParser: true}).then(()=>{
+    const app = express();
+    app.use(session({
+        secret : "caAPISecret",
+        saveUninitialized: false,
+        resave: false
+    }));
+    app.use(express.json());
+    app.use(cors({credentials: true, origin: process.argv[2]}));
+    app.use("/api", routes);
+    
+    app.listen(3000, ()=>{
+        console.log("CA API started on port 3000, test using http://localhost:3000");
+    });
+});*/
